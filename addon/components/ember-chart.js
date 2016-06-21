aa83ed4fcefcd885949bbb39880cde50eed3055b
Sean Stephens <seanstephens29@gmail.com>
2016-06-21 12:09:37 -0600

    fixed blueprint path

--- addon/components/ember-chart.js
+++ addon/components/ember-chart.js
@@ -1,37 +1,50 @@
+/**
+ * @module components
+ *
+ */
 import Ember from 'ember';
 /* global Chart */
 
-export default Ember.Component.extend({
-  tagName: 'canvas',
-  attributeBindings: ['width', 'height'],
+/**
+ * `EmberChart`
+ *
+ */
+export default Ember.Component.extend(
+{
+	tagName: 'canvas',
+	attributeBindings: ['width', 'height'],
 
-  didInsertElement: function(){
-    var context = this.get('element');
-    var data    = this.get('data');
-    var type    = this.get('type');
-    var options = this.get('options');
+	setup: Ember.on('didInsertElement', function()
+	{
+		const context = this.get('element');
+		const data    = this.get('data');
+		const type    = this.get('type');
+		const options = this.get('options');
 
-    var chart = new Chart(context, {
-      type: type,
-      data: data,
-      options: options
-    });
-    this.set('chart', chart);
-    this.addObserver('data', this, this.updateChart);
-    this.addObserver('data.[]', this, this.updateChart);
-    this.addObserver('options', this, this.updateChart);
-  },
+		const chart = new Chart(context, {
+		  type: type,
+		  data: data,
+		  options: options
+		});
 
-  willDestroyElement: function(){
-    this.get('chart').destroy();
-    this.removeObserver('data', this, this.updateChart);
-    this.removeObserver('data.[]', this, this.updateChart);
-  },
+		this.set('chart', chart);
+		this.addObserver('data', this, this.updateChart);
+		this.addObserver('data.[]', this, this.updateChart);
+		this.addObserver('options', this, this.updateChart);
+	}),
 
-  updateChart: function(){
-    var chart   = this.get('chart');
-    var data    = this.get('data');
-    chart.config.data = data;
-    chart.update();
-  }
+	teardown: Ember.on('willDestroyElement', function()
+	{
+		this.get('chart').destroy();
+		this.removeObserver('data', this, this.updateChart);
+		this.removeObserver('data.[]', this, this.updateChart);
+	}),
+
+	updateChart()
+	{
+		var chart = this.get('chart');
+		var data = this.get('data');
+		chart.config.data = data;
+		chart.update();
+	}
 });
